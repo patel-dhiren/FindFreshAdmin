a701cfb62ad6fca9e3f747bd4f0dd0acfe0fca24
Dhiren Patel <swe.dhiren@gmail.com>
2024-03-18 00:25:45 +0530

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: const FirebaseOptions(
-        apiKey: 'AIzaSyCAAvai19QRsN8T7_j01h6Xz7dkJkd8deQ',
-        appId: '1:425734009309:android:7f2d9c5e14352881658634',
-        messagingSenderId: '425734009309',
-        projectId: 'fir-example-app-69765',
-        storageBucket: 'fir-example-app-69765.appspot.com'
+        apiKey: 'AIzaSyAUzqLJ8bNaFpW9I5EAFNaANB0DUWX2X78',
+        appId: '1:651772940713:android:0e1a278a869d4d3965b4b8',
+        messagingSenderId: '651772940713',
+        projectId: 'groceryappmultivendor-53e5b',
+        storageBucket: 'groceryappmultivendor-53e5b.appspot.com'
     ),
   );
   runApp(MyApp());
